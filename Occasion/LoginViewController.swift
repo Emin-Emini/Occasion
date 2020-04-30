2e047366aa627039c0b96bf7cf4452e929a4aa03
Lumbardh Elshani <62339524+lumbardhelshaniNOVAIMS@users.noreply.github.com>
2020-04-30 18:56:26 +0100

    Testing Login

---
+++ Occasion/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Occasion
+//
+//  Created by Lumbardh Elshani on 4/30/20.
+//  Copyright © 2020 Emin Emini. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
